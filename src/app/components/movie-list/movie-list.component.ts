21b3e6aa33cc673f0ec5e6f5b9bf9d4feb6208a0
delmaf <roman.tavaluc@gmail.com>
2025-02-03 21:01:25 +0200

    components has been created

---
+++ src/app/components/movie-list/movie-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-movie-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css'],
+})
+export class MovieListComponent {
+
+}
---
+++ src/app/components/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-movie',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css'],
+})
+export class MovieComponent {
+}
